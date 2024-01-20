6d77f1af37bafc8d3e2660563619a78885adae0a
Mohammad Wasi <150406862+MohammadWasi12345@users.noreply.github.com>
2024-01-19 21:36:37 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+ 
+//question 1 
+let age= 19;
+let student= true;
+console.log('my age is:',age);
+console.log('i am a student:',student);
+
+
+//question 2 area calculator
+let length= 4;
+let width=2;
+
+let area= length*width;
+console.log(area);
+
+//question 3 
+
+let userName= prompt('what is your name');
+
+
+alert(`welcometo our website ${userName} hope you are fine`);
